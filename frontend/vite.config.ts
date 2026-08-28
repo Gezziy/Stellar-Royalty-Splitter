caff3a2e5d76e5410b12ea282662dbeef31f1b1a
Fuwad Busari <114140933+cythecode@users.noreply.github.com>
2026-08-28 15:59:04 +0400

    feat: Analyze and optimize frontend bundle size (#834)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:3001",
+  plugins: [react(), visualizer()],
+  server: { proxy: { "/api": "http://localhost:3001" } },
+  build: {
+    chunkSizeWarningLimit: 300,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) return id.includes("recharts") ? "recharts" : "vendor";
+        },
+      },
     },
   },
 });
